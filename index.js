735046b0a12fcf88b050724c537857426afffaa1
Dani Lyn <dlyn1110@gmail.com>
2019-02-13 19:23:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// let store = {drivers: [], passsengers: [], trips: []};
-//
-// let driverId = 0;
-//
-// class Driver{
-//   constructor(name) {
-//     this.name = name
-//     this.id = ++driverId
-//     store.drivers.push(this)
-//   }
-// trips() {
-//   return store.trips.filter(trip => {
-//     return trip.driverID == this.id
-//   })
-// }
-
-
 let store = {drivers: [], passengers: [], trips: []}
 
 let driverId = 0
