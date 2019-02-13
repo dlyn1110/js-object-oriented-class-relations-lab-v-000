640177a328d2e978370b48acedfe5cf48faa52c4
Dani Lyn <dlyn1110@gmail.com>
2019-02-13 19:13:48 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,11 @@
+let store = {drivers: [], passsengers: [], trips: []};
+
+let driverId = 0;
+
+class Driver{
+  constructor(name) {
+    this.name = name
+    this.id = ++driverId
+    store.drivers.push(this)
+  }
+}
